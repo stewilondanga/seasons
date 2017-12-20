118a380e49dc5a3bbeebc34020c0636a24398cca
Stewart ilondanga <stilondanga@gmail.com>
2017-12-20 21:00:07 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,12 @@ var days = [
    "Friday",
    "Saturday"
 ];
+
+var d = new Date();
+var date = document.getElementById('date');
+var date_day = document.getElementById('day');
+var date_month = document.getElementById('month-year');
+
+date_day.innerHTML = days[d.getDay()];
+date.innerHTML = d.getDate();
+date_month.innerHTML = months[d.getMonth()] + " " + d.getFullYear();
